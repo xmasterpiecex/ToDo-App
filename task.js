0d8100a705b031f091cef1e98c92dfceb3554b9d
Alex <a211019971420125@gmail.com>
2023-06-14 00:39:10 +0300

    ToDo-7: Add deleting items functionality

--- task.js
+++ task.js
@@ -1,7 +1,7 @@
 const tasksConteiner = document.getElementById('tasks');
+const creationTitle = document.getElementById('creationTitle');
 const buttonAccept = document.getElementById('accept');
 const buttonPriority = document.getElementById('priority');
-const creationTitle = document.getElementById('creationTitle');
 
 const tasksList = [];
 
@@ -47,7 +47,7 @@ function createTask() {
     .forEach(
       (task) =>
         (tasksConteiner.innerHTML += `
-      <div class="card">
+      <div class="card" id="${task.id}">
         <button class="action-button">
             <img src="./assets/svg/pririty.svg" alt="" class="svg" />
         </button>
@@ -61,7 +61,7 @@ function createTask() {
             <button class="action-button small">
                 <img src="./assets/svg/arrowup.svg" alt="" class="svg small-svg" />
             </button>
-            <button class="action-button small">
+            <button class="action-button small" id="delete">
                 <img src="./assets/svg/delete.svg" alt="" class="svg small-svg" />
             </button>
             <button class="action-button small">
@@ -76,6 +76,24 @@ function createTask() {
       `)
     );
 
+  deleteAction();
+
   creationTitle.value = '';
   switchCreationButtonState();
+  console.log(tasksList);
+}
+
+function deleteAction() {
+  const buttonDelete = document.querySelectorAll('#delete');
+
+  buttonDelete.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      const perentNode = btn.closest('.card');
+      const id = Number(perentNode.id);
+      const index = tasksList.findIndex((task) => task.id == id);
+      tasksList.splice(index, 1);
+      perentNode.remove();
+      console.log(tasksList, index, id);
+    });
+  });
 }
